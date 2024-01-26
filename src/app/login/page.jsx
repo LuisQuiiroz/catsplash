4b66b2f6be37be4f22dcafbe641437077bf31f76
Luis Quiroz <luisdev99@gmail.com>
2024-01-26 14:37:47 -0600

    fix form styles

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -27,28 +27,29 @@ export default function Login () {
   }
   return (
     <div className='flex items-center justify-center h-screen'>
-      <form
-        className='mx-auto block p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 '
-        onSubmit={handleSubmit}
-        method='post'
-      >
-        <h1 className='mb-4 text-2xl font-medium text-gray-900 dark:text-white'>{title}</h1>
-        <div className='mb-5 w-80'>
-          <label htmlFor='email' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Your email</label>
-          <input type='email' id='email' name='email' className=' bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' placeholder='name@gmail.com' required defaultValue='test@test.com' />
-        </div>
-        <div className='mb-5'>
-          <label htmlFor='password' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Your password</label>
-          <input type='password' id='password' name='password' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' required defaultValue='test' />
-        </div>
-        <div className='flex items-start mb-5'>
-          <div className='flex items-center h-5'>
-            <input id='remember' type='checkbox' value='' className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800' />
+      <div className='w-96 mx-auto block bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+        <form
+          className='p-6'
+          onSubmit={handleSubmit}
+          method='post'
+        >
+          <h1 className='mb-4 text-2xl font-medium text-gray-900 dark:text-white'>{title}</h1>
+          <div className='mb-5'>
+            <label htmlFor='email' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Your email</label>
+            <input type='email' id='email' name='email' className=' bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' placeholder='name@gmail.com' required defaultValue='test@test.com' />
           </div>
-          <label htmlFor='remember' className='ms-2 text-sm font-medium text-gray-900 dark:text-gray-300'>Remember me</label>
-        </div>
-        <button type='submit' className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Submit</button>
-        {
+          <div className='mb-5'>
+            <label htmlFor='password' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Your password</label>
+            <input type='password' id='password' name='password' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' required defaultValue='test' />
+          </div>
+          <div className='flex items-start mb-5'>
+            <div className='flex items-center h-5'>
+              <input id='remember' type='checkbox' value='' className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800' />
+            </div>
+            <label htmlFor='remember' className='ms-2 text-sm font-medium text-gray-900 dark:text-gray-300'>Remember me</label>
+          </div>
+          <button type='submit' className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Submit</button>
+          {
           isLogin
             ? (
               <p id='helper-text' className='mt-6 text-sm text-gray-500 dark:text-gray-400 text-center'>Don’t have an account yet? <Link href='/register' className='font-medium text-blue-600 hover:underline dark:text-blue-500'>Register</Link>.</p>
@@ -59,7 +60,8 @@ export default function Login () {
               )
         }
 
-      </form>
+        </form>
+      </div>
     </div>
   )
 }
