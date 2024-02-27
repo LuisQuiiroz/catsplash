6af6b52679085782fca3c1cdb3075e23f309eee9
Luis Quiroz <luisdev99@gmail.com>
2024-02-27 16:41:52 -0600

    listPost is now reusable

--- src/components/posts/ListPost.jsx
+++ src/components/posts/ListPost.jsx
@@ -1,17 +1,17 @@
-import { prisma } from '@/libs/prisma'
 import { Post } from './Post'
-async function loadPosts () {
-  return await prisma.post.findMany()
-}
-export async function ListPost () {
-  const posts = await loadPosts()
-
+export async function ListPost ({ data }) {
   return (
     <div className='columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-4 md:gap-8 max-w-screen-xl mx-auto'>
       {
-        posts.map(post => (
-          <Post key={post.id} post={post} />
-        ))
+        data?.error || data?.length === 0
+          ? (
+            <h1 className='text-3xl text-gray-500 dark:text-gray-400'>No posts found</h1>
+            )
+          : (
+              data.map(post => (
+                <Post key={post.id} post={post} />
+              ))
+            )
       }
     </div>
   )
