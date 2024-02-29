d924ccec9f84193367ffb9f2a0dfff82230a682d
Luis Quiroz <luisdev99@gmail.com>
2024-02-29 16:12:33 -0600

    add Link instead of using a button

--- src/components/user/UserOptions.jsx
+++ src/components/user/UserOptions.jsx
@@ -2,29 +2,17 @@ import { signOut, useSession } from 'next-auth/react'
 import { createPortal } from 'react-dom'
 import { PostForm } from '../posts/PostForm'
 import { useState } from 'react'
-import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 export function UserOptions () {
   const [showModal, setShowModal] = useState(false)
   const [UserOptions, setUserOptions] = useState(false)
-
-  const router = useRouter()
 
   const { data: session } = useSession()
   const username = session?.user?.name
   const imgUser = session?.user?.image
 
   const toggleMenu = () => setUserOptions(value => !value)
-
-  const handleProfile = () => {
-    toggleMenu()
-    router.push(`/profiles/${username}`)
-  }
-
-  const handleAccount = () => {
-    toggleMenu()
-    router.push('/settings/profile')
-  }
 
   return (
     <div className='flex items-center gap-3 md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse'>
@@ -52,24 +40,21 @@ export function UserOptions () {
           <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className={`size-4 ${UserOptions ? 'rotate-180' : ''}`}>
             <path strokeLinecap='round' strokeLinejoin='round' d='m19.5 8.25-7.5 7.5-7.5-7.5' />
           </svg>
-
         </button>
         {UserOptions && (
-          <div className='absolute right-0 mt-2 p-2 w-48 bg-white dark:bg-gray-900 rounded-md shadow-xl z-20 font-medium border dark:border-gray-800'>
-            <button
-              type='button'
+          <div className='absolute right-0 mt-2 p-2 w-48 bg-white dark:bg-gray-900 rounded-md shadow-xl z-20 font-medium border dark:border-gray-800' onClick={toggleMenu}>
+            <Link
+              href={`/profiles/${username}`}
               className='block px-4 py-2 text-gray-800 hover:bg-gray-300 dark:text-white dark:hover:bg-gray-800 rounded-md w-full text-left'
-              onClick={handleProfile}
             >
               Profile
-            </button>
-            <button
-              type='button'
+            </Link>
+            <Link
+              href='/settings/profile'
               className='block px-4 py-2 text-gray-800 hover:bg-gray-300 dark:text-white dark:hover:bg-gray-800 rounded-md w-full text-left'
-              onClick={handleAccount}
             >
               Account
-            </button>
+            </Link>
             <button
               type='button'
               className='block px-4 py-2 text-red-600 hover:bg-gray-300 dark:hover:bg-gray-800 rounded-md w-full text-left'
