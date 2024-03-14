d809f07cf1fcac92a83b4f0756e6046bf6538a6b
Luis Quiroz <luisdev99@gmail.com>
2024-03-14 14:28:33 -0600

    max width in main

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import { getAllPosts } from './utils/PostRequests'
 export default async function Home () {
   const data = await getAllPosts()
   return (
-    <main className='p-4 '>
+    <main className='p-4 max-w-screen-xl mx-auto'>
       <ListPost data={data} />
     </main>
 
