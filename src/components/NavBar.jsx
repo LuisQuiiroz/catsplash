4cd56213765f503a27bea8b4b5a8196a8a379622
Luis Quiroz <luisdev99@gmail.com>
2024-03-14 14:38:24 -0600

    search bar removed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,7 +18,7 @@ export function NavBar () {
           <Link href='/' className='flex items-center space-x-3 rtl:space-x-reverse'>
             <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>catsplash</span>
           </Link>
-          <div className='flex '>
+          {/* <div className='flex '>
             <button type='button' data-collapse-toggle='navbar-search' aria-controls='navbar-search' aria-expanded='false' className='md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1'>
               <svg className='w-5 h-5' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 20 20'>
                 <path stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z' />
@@ -50,12 +50,13 @@ export function NavBar () {
               </div>
               <input type='text' id='search-navbar' className='block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' placeholder='Search...' />
             </div>
-            {
+          </div> */}
+
+          {
               session?.user
                 ? <UserOptions />
                 : <GuestOptions />
               }
-          </div>
         </div>
       </nav>
     </>
