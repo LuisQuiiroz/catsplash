58fad1cb3559b05a9862d0a386db8a2c9f9bc6ce
Luis Quiroz <luisdev99@gmail.com>
2024-04-08 17:11:38 -0600

    delete console.log

--- src/app/images/[id]/page.jsx
+++ src/app/images/[id]/page.jsx
@@ -14,7 +14,6 @@ export default async function Image ({ params }) {
   try {
     data = await getOnePost(id)
     user = await GetOneUser(data?.userId)
-    console.log(data)
   } catch (error) {
     data = error
   }
