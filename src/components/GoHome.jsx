2dd93fb108c2a81112d25052957759993438dff1
Luis Quiroz <luisdev99@gmail.com>
2024-04-08 16:57:58 -0600

    GoHome component

---
+++ src/components/GoHome.jsx
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+
+export function GoHome () {
+  return (
+    <Link className=' text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700' href='/'> Back to Homepage
+    </Link>
+  )
+}
